51372ceeb05f25c1f618dcdf26e22a724d5d35cd
Levan Sakvarelidze <l.sakvarelidze@hotmail.com>
2022-12-02 12:09:15 +0400

    bla

--- src/UI/Card.js
+++
@@ -1,7 +0,0 @@
-import styles from './Card.module.css'
-
-const Card = props => {
-    return <div className={props.section === "add" ? styles.add : props.section === "top" ? styles.top : props.section === "bottom" ? styles.bottom : styles.normal}>{props.children}</div>
-}
-
-export default Card;
